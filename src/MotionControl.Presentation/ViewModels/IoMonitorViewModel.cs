661865e8895e3b9f66a81bc706b8406bc0e76253
root <root@localhost.localdomain>
2026-04-20 19:16:06 +0800

    Force IO Monitor refresh by rebuilding Inputs/Outputs collections each UI refresh

--- src/MotionControl.Presentation/ViewModels/IoMonitorViewModel.cs
+++ src/MotionControl.Presentation/ViewModels/IoMonitorViewModel.cs
@@ -1,33 +1,45 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using MotionControl.Control.Services;
 using MotionControl.Domain.Entities;
 
 namespace MotionControl.Presentation.ViewModels;
 
-public sealed class IoMonitorViewModel
+public sealed class IoMonitorViewModel : INotifyPropertyChanged
 {
     private readonly Machine _machine;
+    private readonly IoControlService _ioControlService;
 
     public IoMonitorViewModel(Machine machine, IoControlService ioControlService)
     {
         _machine = machine;
-        Inputs = new ObservableCollection<IoPointViewModel>(_machine.IoPoints.Where(io => !io.IsOutput).Select(io => new IoPointViewModel(io, ioControlService)));
-        Outputs = new ObservableCollection<IoPointViewModel>(_machine.IoPoints.Where(io => io.IsOutput).Select(io => new IoPointViewModel(io, ioControlService)));
+        _ioControlService = ioControlService;
+        Inputs = BuildInputs();
+        Outputs = BuildOutputs();
     }
 
-    public ObservableCollection<IoPointViewModel> Inputs { get; }
-    public ObservableCollection<IoPointViewModel> Outputs { get; }
+    public ObservableCollection<IoPointViewModel> Inputs { get; private set; }
+    public ObservableCollection<IoPointViewModel> Outputs { get; private set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     public void RefreshAll()
     {
-        foreach (var input in Inputs)
-        {
-            input.Refresh();
-        }
+        Inputs = BuildInputs();
+        Outputs = BuildOutputs();
+        OnPropertyChanged(nameof(Inputs));
+        OnPropertyChanged(nameof(Outputs));
+    }
 
-        foreach (var output in Outputs)
-        {
-            output.Refresh();
-        }
+    private ObservableCollection<IoPointViewModel> BuildInputs()
+        => new(_machine.IoPoints.Where(io => !io.IsOutput).Select(io => new IoPointViewModel(io, _ioControlService)));
+
+    private ObservableCollection<IoPointViewModel> BuildOutputs()
+        => new(_machine.IoPoints.Where(io => io.IsOutput).Select(io => new IoPointViewModel(io, _ioControlService)));
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
