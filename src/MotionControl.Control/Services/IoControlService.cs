da0117141012556769ebe9a4f5536db8a6474f1f
root <root@localhost.localdomain>
2026-04-21 14:14:43 +0800

    Record DO changes in IO event log on successful output write

--- src/MotionControl.Control/Services/IoControlService.cs
+++ src/MotionControl.Control/Services/IoControlService.cs
@@ -1,12 +1,32 @@
 using MotionControl.Device.Abstractions.Controllers;
 using MotionControl.Device.Abstractions.Results;
+using MotionControl.Domain.Entities;
 
 namespace MotionControl.Control.Services;
 
-public sealed class IoControlService(IIoController motionController)
+public sealed class IoControlService(IIoController motionController, Machine machine, IoEventRuntimeState ioEventRuntimeState)
 {
-    public Task<DeviceResult> SetOutputAsync(int address, bool value, CancellationToken cancellationToken = default)
+    public async Task<DeviceResult> SetOutputAsync(int address, bool value, CancellationToken cancellationToken = default)
     {
-        return motionController.SetIoPointValueAsync(address, value, cancellationToken);
+        var result = await motionController.SetIoPointValueAsync(address, value, cancellationToken);
+        if (!result.Success)
+        {
+            return result;
+        }
+
+        var ioPoint = machine.IoPoints.FirstOrDefault(x => x.IsOutput && x.Address == address);
+        if (ioPoint is not null)
+        {
+            ioEventRuntimeState.Add(new IoEventRecord
+            {
+                Name = ioPoint.Name,
+                Address = ioPoint.Address,
+                IsOutput = true,
+                Value = value,
+                Message = $"{ioPoint.Name} -> {(value ? "ON" : "OFF")}"
+            });
+        }
+
+        return result;
     }
 }
