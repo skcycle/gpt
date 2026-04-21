b22b249964d9017405a2a9d4730c1b780f70753c
root <root@localhost.localdomain>
2026-04-21 21:20:22 +0800

    Fix missing CylinderState enum import in cylinder view model

--- src/MotionControl.Presentation/ViewModels/CylinderItemViewModel.cs
+++ src/MotionControl.Presentation/ViewModels/CylinderItemViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using MotionControl.Control.Services;
 using MotionControl.Domain.Entities;
+using MotionControl.Domain.Enums;
 using MotionControl.Presentation.Commands;
 
 namespace MotionControl.Presentation.ViewModels;
